db40c91c9da3acce8bb352d33705f259204760b6
LeonisCuriosity <leonis.curiosity@mdl29.net>
2015-10-18 23:53:02 +0200

    différent en fonction des pages

--- client/index.php
+++ client/index.php
@@ -8,15 +8,30 @@
         <header>
             <ul>
                 <li><a href="index.php?page=Home">Accueil</a></li>
-                <li><a href="index.php?page=IUOP">IUOP</a></li>
+                <li><a href="index.php?page=Tas">Trucs à Savoir</a></li>
                 <li><a href="index.php?page=Projets">Projets</a></li>
-                <li><a href="index.php?page=CmC">ÇmC?</a></li>
+                <li><a href="index.php?page=CmC">Ça Marche Comment?</a></li>
                 <li><a href="index.php?page=About">À propos</a></li>
             </ul>
         </header>
         <section id="contexte">
             <?php
-                print $_GET['page'];
+            switch ($_GET['page']) {
+                case "Tas":
+                    echo "C'est la page des trucs à savoir";
+                break;
+                case "Projets":
+                    echo "C'est la page des projets";
+                break;
+                case "Cmc":
+                    echo "C'est la rubrique : ça marche comment?";
+                break;
+                case "About":
+                    echo "C'est la rubrique à propos de moi";
+                break;
+                default:
+                    echo "Accueil";
+            }
             ?>
         </section>
         <footer>
